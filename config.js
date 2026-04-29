d5b310e446ceffa7b3505d71181ac7157bf1750f
steadybb <steadybomber419@gmail.com>
2026-04-29 10:15:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// config.js – safe version
 module.exports = {
-  token: 'MTM2NTgzNTA2MzQwODcyNjEyNw.GommCI.WswzvH7jyR2o1FioSTJXUp0bxgnlKont1Kh5ug',
-  clientId: '1365835063408726127',
-  guildId: '1361129852068958208'
+  token: process.env.DISCORD_TOKEN,
+  clientId: process.env.CLIENT_ID,
+  guildId: process.env.GUILD_ID
 };
